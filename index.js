639f74bc3a2be330b8afca17c4349779563ee8d1
KubaDC <67146918+KubaDC@users.noreply.github.com>
2020-06-19 12:38:57 +0200

    me

---
+++ index.js
@@ -0,0 +1,56 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const token = 'NzIzMTE0MDQ0ODc3MzA3OTI1.Xus6jA.59bT1MadINzmGaqkH-qy-kMWxK8';
+
+
+var servers =  {};
+bot.on('ready', () =>{
+    console.log('This bot is online!');
+})
+
+bot.on('message', msg=>{
+    if (msg.content === "HELLO"){
+        msg.reply('Hello Friend!'); 
+    }
+})
+bot.on('message', msg=>{
+    if (msg.content === "BYE"){
+        msg.reply('Bye Friend!'); 
+    }
+})
+bot.on('message', msg=>{
+    if (msg.content === "Hello"){
+        msg.reply('Hello Friend!'); 
+    }
+})
+bot.on('message', msg=>{
+    if (msg.content === "Bye"){
+        msg.reply('Bye Friend!'); 
+    }
+})
+bot.on('message', msg=>{
+    if (msg.content === "hello"){
+        msg.reply('Hello Friend!'); 
+    }
+})
+bot.on('message', msg=>{
+    if (msg.content === "bye"){
+        msg.reply('Bye Friend!'); 
+    }
+})
+bot.on('message', msg=>{
+    if (msg.content === "HI"){
+        msg.reply('Hi Friend!'); 
+    }
+})
+bot.on('message', msg=>{
+    if (msg.content === "Hi"){
+        msg.reply('Hi Friend!'); 
+    }
+})
+bot.on('message', msg=>{
+    if (msg.content === "hi"){
+        msg.reply('Hi Friend!'); 
+    }
+})
+bot.login(token);
